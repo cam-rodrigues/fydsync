ecf7f1fceb89d933de2d930e36671b0ad53f4b4c
Cam <219443440+cam-rodrigues@users.noreply.github.com>
2026-08-06 12:44:05 -0400

    Reduce top padding in sidebar

--- app.py
+++ app.py
@@ -329,7 +329,7 @@ st.markdown(
         }
 
         [data-testid="stSidebar"] > div:first-child {
-            padding-top: 0.5rem;
+            padding-top: 0.3rem;
         }
 
         /* Sidebar logo image */
